86720b0acae5a086c1c3d8db0388edd2ceffb201
Michael Poutre <m1kep.my.mail@gmail.com>
2022-05-24 00:18:38 -0700

    fix: Fix type coerced check

--- secret-actions/js-scripts/test-env-numbers.js
+++ secret-actions/js-scripts/test-env-numbers.js
@@ -3,5 +3,5 @@ const checkUntil = 10
 
 for (let i = 0; i <= checkUntil; i++) {
     envValue = process.env[`SECRET`]
-    console.log(`Value is ${i}: ${envValue == i}`)
+    console.log(`Value is ${i}: ${envValue == i && envValue !== ''}`)
 }
